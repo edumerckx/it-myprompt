537682364cb0acbfe52003611f068b9faa74131b
Eduardo Gomes <edu_merckx@yahoo.com.br>
2025-05-13 00:15:33 -0300

    initial setup

---
+++ it_myprompt/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+
+from it_myprompt.settings import Settings
+
+engine = create_async_engine(Settings().DATABASE_URL)
+
+
+async def get_session():
+    async with AsyncSession(engine) as session:
+        yield session
